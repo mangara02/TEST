b76d2aa9bbedcf77e86cbf6b22bc40b2538e586f
mangara02 <156886447+mangara02@users.noreply.github.com>
2024-04-28 18:13:35 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+def main():
+    st.title("Drug Quantity Prediction App")
+    st.write("This app predicts the quantity of drugs to be ordered based on historical data.")
+
+    model = joblib.load('xgb.pkl')
+  
+    # Date input
+    date_input = st.date_input("Select a date for prediction", pd.to_datetime('today'))
+
+    # Drug Brands input
+    drug_brands_options = label_encoder_drugs.classes_
+    selected_drug_brand = st.selectbox("Select Drug Brand", drug_brands_options)
+
+    # Branch input
+    branch_options = label_encoder_branch.classes_
+    selected_branch = st.selectbox("Select Branch", branch_options)
+
+    # Month input
+    selected_month = pd.to_datetime(date_input).month
+
+    # Prediction logic
+    input_data = pd.DataFrame({
+        'month': [selected_month],
+        'Drug Brands': [selected_drug_brand],
+        'Branch': [selected_branch]
+    })
+
+    input_data_o = input_data.apply(pd.to_numeric, errors='coerce')
+    input_matrix = xgb.DMatrix(input_data_o)
+
+    prediction = model.predict(input_matrix)
+
+    st.write("Predicted Quantity:", prediction[0])
+
+if __name__ == '__main__':
+    main()

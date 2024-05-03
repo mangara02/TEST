bbe6802c316bb992f93ca9a6093fc2d3fa463c3a
mangara02 <156886447+mangara02@users.noreply.github.com>
2024-05-03 20:59:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import xgboost as xgb
 import joblib
 
 label_encoder_drugs = joblib.load('led.pkl')
@@ -12,26 +11,19 @@ def main():
     st.title("Drug Quantity Prediction App")
     st.write("This app predicts the quantity of drugs to be ordered based on historical data.")
 
-    # Drug Brands input
     drug_brands_options = label_encoder_drugs.classes_
     selected_drug_brand = st.selectbox("Select Drug Brand", drug_brands_options)
 
-    # Branch input
     branch_options = label_encoder_branch.classes_
     selected_branch = st.selectbox("Select Branch", branch_options)
 
-    # Month input
     selected_month = st.slider("Select a month", 1, 12, 1)
 
-    # Prediction logic
     input_data = pd.DataFrame({
         'Drug Brands': [selected_drug_brand],
         'Branch': [selected_branch],
         'month': [selected_month]
     })
-
-    input_data_o = input_data.apply(pd.to_numeric, errors='coerce')
-    input_matrix = xgb.DMatrix(input_data_o)
 
     prediction = model.predict(input_matrix)
     rounded_prediction = np.ceil(prediction[0])
